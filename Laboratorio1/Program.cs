0f3c546278ceb99a0a87e00b1953a49301c3e26e
Hoffmann <Francine.Hoffmann@dellteam.com>
2022-07-30 19:18:31 -0300

    Introdução, Laboratório 1 e tipos anuláveis

---
+++ Laboratorio1/Program.cs
@@ -0,0 +1,44 @@
+﻿byte b;
+b = byte.MaxValue;
+Console.WriteLine("Valor máximo de byte:" + b);
+Console.WriteLine($"Valor máximo de bytes: {b}");
+long l;
+l = long.MaxValue;
+Console.WriteLine("Valor máximo de long:" + l);
+int i;
+i = int.MaxValue;
+Console.WriteLine("Valor máximo de int:" + i);
+
+string strPrimeira = "Alô";
+string strSegunda = "Mundo";
+string strTerceira = strPrimeira +strSegunda;
+Console.WriteLine(strTerceira);
+/* ToString converte para string*/
+int cchTamanho = strTerceira.Length;
+string strQuarta = "Tamanho = " + cchTamanho.ToString();
+Console.WriteLine(strQuarta);
+/* Datetime é uma structy ou seja é um tipo valor diferente de Java, tem metodos, valores igual uma class --- dt . aparece valios operadores, isso significa que ocorre sobrecarga de operadores */
+DateTime dt = new DateTime(2022,7,19);
+Console.WriteLine(dt.ToShortDateString());
+DateTime agora = DateTime.Now;
+Console.WriteLine(agora);
+
+int a = 10;
+float f = 0;
+f = a;
+Console.WriteLine(f);
+/* valor inteiro é menor que o tipo flutuante ou seja essa conversão implicita ocorre sem perda de dados 
+já no exemplo abaixo a conversão direta ele não faz pois o float é menor que o int tendo perda de dados no processo tem que usar o int (operador de cast que é um operador explicito) na frente pra ele fazer a conversão e perder os dados no processo se der um run o numero ira aparecer no console apenas o 0
+  / 0.5 é um tipo double ai não pode sem usar o f depopis do numero pois float é menor que double*/
+f = 0.5f;
+i = (int)f;
+Console.WriteLine(i);
+
+
+
+
+
+
+
+
+
---
+++ Tiposanulaveis/Program.cs
@@ -0,0 +1,59 @@
+﻿// isto não é possível
+//int x = null;
+//isso é possivel, colocando o interrogação ao lado , estou dizendo que meu objeto pode ser anulavel( int? string?)
+//novos operadores null-forgiving
+int? y = null;
+Console.WriteLine(y);
+string s = null;
+Console.WriteLine(s);
+string? t = null;
+Console.WriteLine(t);
+// qual valor das variaveis nas seguintes declarações?
+//string first; (valor default = null)
+//string second = string.Empty; ( aqui foi inicializada explicitamente, nunca será null)
+//int third; (valor default de int = 0 )
+//DateTime date; (valor default de datetime = 00/00/00 Minvalue)
+//int? first; (aqui o valor sera null)
+//int? second = null; (aqui null)
+//int? third = default; (aqui null)
+//int? fourth = new(); (aqui 0)
+//Sempre inicializar quando colcoar uma variavel, pois tem essas particularidades e o programador que vier depois para fazer a manutenção pode não saber e criar um bug
+//string first = string.Empty; ( se foi inicializada o compialdor nunca entendera que é null ,. nunca será null)
+//string second;  ( valor default de um tipo referencia é um valor null, mas recebera um warning para alertar que esqueceu de inicializar, inicialmente será null )
+//string? third; ( aqui pode ser null)
+int? a = 28;
+int b = a ?? 0;
+Console.WriteLine(b);
+// operador null Coalesing ?? b sera 28 pois a foi inicializado com 28
+// operador null forgiving ! apos a inicialização é null o valor e vou usar
+int? c = null;
+int d = c ?? 0;
+Console.WriteLine(d);
+// nesse null coalesing será 0 , pois c é null então pegará o 0 c??0 significa pegue o valor de c  ou 0, como c é null ele assume o valor 0 da expressão
+int? e = a + c;
+Console.WriteLine(e);
+// qualquer valor somado com null vira null
+string st = null!;
+Console.WriteLine(st);
+// exclamação ali null forgiving, esta dizendo que aquela expressão pode ser nula
+DateTime? dt = DateTime.Now;
+Console.WriteLine(dt?.Day);
+// null condicional, se não colocar o interrogação no dt?.day ele não deixa pois day não aceita o datetime?, porem usando null condicional ele vai pois ele diz "compilador se dt for diferente de null , pegue o day se ela for null o valor da expressão será null"
+if (dt != null){
+    Console.WriteLine(dt?.Day);
+}
+if (dt.HasValue){
+    Console.WriteLine(dt.Value.Day);
+    Console.WriteLine(dt.HasValue);
+}
+//todas as expressoes são a mesma coisa so de maneira diferente no terceiro exemplo o has value é um boleano, vai verificar se o valor de dt é null se for ele não realiza a segunda linha , se for ele printa
+
+
+
+
+
+
+
+
+
+
