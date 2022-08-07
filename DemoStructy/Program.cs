c7cdb7b15114b0435daa03271d8ff88c09e1d35f
Francine Broday Hoffmann <francine.bhoffmann@gmail.com>
2022-08-06 23:14:44 -0300

    operadores  e sobrecarga de operador

---
+++ DemoStructy/Program.cs
@@ -0,0 +1,7 @@
+﻿Fracao f1 = new Fracao(1,2);
+Fracao f2 = new Fracao(3,4);
+Console.WriteLine(f1.FracaoPorExtenso());
+Console.WriteLine(f2.Numerador);
+Fracao f3 = f1 * f2;
+// isso não funcionaria se não fizesse a sobre carga do operador , pois o objeto não supporta essa multiplicação
+Console.WriteLine(f3.FracaoPorExtenso());
